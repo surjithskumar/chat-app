4235d9c811b91c6bf037304c8370fabff122fa7c
SURJITH S KUMAR <surjithskumar23@gmail.com>
2024-11-19 22:36:25 +0530

    search functionality

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './LeftSidebar.css'
 import assets from '../../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import { AppContext } from '../../context/AppContext'
+import { db } from '../../config/firebase'
 
 const LeftSidebar = () => {
 
   const navigate = useNavigate();
+
+  const {userData} = useContext(AppContext);
+
+  const inputHandler = async (e)=> {
+    try {
+      const input = e.target.value;
+      const userRef = collection(db,'users');
+      const q = query(userRef,where("username","==",input.toLowerCase()));
+      const querySnap = await getDocs(q);
+      if(!querySnap.empty && querySnap.docs[0].data().id !== userData.id){
+        console.log(querySnap.docs[0].data());
+      }
+    } catch (error) {
+      
+    }
+  }
 
   return (
     <div className='ls'>
@@ -23,7 +42,7 @@ const LeftSidebar = () => {
         </div>
         <div className="ls-search">
             <img src={assets.search_icon} alt="" />
-            <input type="text" placeholder='Search Here' />
+            <input onChange={inputHandler} type="text" placeholder='Search Here' />
         </div>
       </div>
       <div className="ls-list">
